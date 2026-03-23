607d97291379677ee41e62d392a8040240ddf830
Amie. ✨ <158010091+saraniz@users.noreply.github.com>
2026-03-23 22:16:43 +0530

    fix

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/sh
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt  #grep is search tool and -i igonore the uppercase/lowercase difference.so it search word dragon in dragon..txt
